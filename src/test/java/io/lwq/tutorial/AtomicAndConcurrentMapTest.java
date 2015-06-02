70d73ed75c70fdc71c3b8e88268fcdfa4325d794
Weiquan LONG <longweiquan@gmail.com>
2015-06-02 14:23:36 +0200

    Add tutorial

---
+++ src/test/java/io/lwq/tutorial/AtomicAndConcurrentMapTest.java
@@ -0,0 +1,43 @@
+package io.lwq.tutorial;
+
+import org.junit.Test;
+
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+/**
+ * Refer to
+ *
+ * http://winterbe.com/posts/2015/05/22/java8-concurrency-tutorial-atomic-concurrent-map-examples/
+ */
+public class AtomicAndConcurrentMapTest {
+
+    @Test
+    public void testAtomicInteger() throws InterruptedException {
+        AtomicInteger atomicInt = new AtomicInteger(0);
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        IntStream.range(0, 1000)
+            .forEach(i -> executor.submit(atomicInt::incrementAndGet));
+
+        executor.awaitTermination(10, TimeUnit.SECONDS);
+
+        System.out.println(atomicInt.get());    // => 1000
+    }
+
+    @Test
+    public void testConcurrentMap(){
+        ConcurrentMap<String, String> map = new ConcurrentHashMap<>();
+        map.put("foo", "bar");
+        map.put("han", "solo");
+        map.put("r2", "d2");
+        map.put("c3", "p0");
+
+        map.forEach((key, value) -> System.out.printf("%s = %s\n", key, value));
+
+        map.merge("foo", "boo", (oldVal, newVal) -> newVal + " was " + oldVal);
+        System.out.println(map.get("foo"));   // boo was foo
+    }
+}
---
+++ src/test/java/io/lwq/tutorial/LockTest.java
@@ -0,0 +1,163 @@
+package io.lwq.tutorial;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.StampedLock;
+import java.util.stream.IntStream;
+
+/**
+ * Reference to
+ *
+ * http://winterbe.com/posts/2015/04/30/java8-concurrency-tutorial-synchronized-locks-examples/
+ */
+public class LockTest {
+
+    @Test
+    public void testReentrantLock(){
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        ReentrantLock lock = new ReentrantLock();
+
+        executor.submit(() -> {
+            lock.lock();
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        });
+
+        executor.submit(() -> {
+            System.out.println("Locked: " + lock.isLocked());
+            System.out.println("Held by me: " + lock.isHeldByCurrentThread());
+            boolean locked = lock.tryLock();
+            System.out.println("Lock acquired: " + locked);
+        });
+    }
+
+    @Test
+    public void testReadWriteLock(){
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        Map<String, String> map = new HashMap<>();
+        ReadWriteLock lock = new ReentrantReadWriteLock();
+
+        executor.submit(() -> {
+            lock.writeLock().lock();
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                map.put("foo", "bar");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.writeLock().unlock();
+            }
+        });
+    }
+
+    @Test
+    public void testStampedLock(){
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        Map<String, String> map = new HashMap<>();
+        StampedLock lock = new StampedLock();
+
+        executor.submit(() -> {
+            long stamp = lock.writeLock();
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                map.put("foo", "bar");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlockWrite(stamp);
+            }
+        });
+
+        Runnable readTask = () -> {
+            long stamp = lock.readLock();
+            try {
+                System.out.println(map.get("foo"));
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlockRead(stamp);
+            }
+        };
+
+        executor.submit(readTask);
+        executor.submit(readTask);
+
+    }
+
+    @Test
+    public void testOptimisticLock(){
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        StampedLock lock = new StampedLock();
+
+        executor.submit(() -> {
+            long stamp = lock.tryOptimisticRead();
+            try {
+                System.out.println("Optimistic Lock Valid: " + lock.validate(stamp));
+                TimeUnit.SECONDS.sleep(1);
+                System.out.println("Optimistic Lock Valid: " + lock.validate(stamp));
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println("Optimistic Lock Valid: " + lock.validate(stamp));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock(stamp);
+            }
+        });
+
+        executor.submit(() -> {
+            long stamp = lock.writeLock();
+            try {
+                System.out.println("Write Lock acquired");
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock(stamp);
+                System.out.println("Write done");
+            }
+        });
+    }
+
+    @Test
+    public void testSemaphore(){
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
+        Semaphore semaphore = new Semaphore(5);
+
+        Runnable longRunningTask = () -> {
+            boolean permit = false;
+            try {
+                permit = semaphore.tryAcquire(1, TimeUnit.SECONDS);
+                if (permit) {
+                    System.out.println("Semaphore acquired");
+                    Thread.sleep(5);
+                } else {
+                    System.out.println("Could not acquire semaphore");
+                }
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            } finally {
+                if (permit) {
+                    semaphore.release();
+                }
+            }
+        };
+
+        IntStream.range(0, 10).forEach(i -> executor.submit(longRunningTask));
+    }
+}
